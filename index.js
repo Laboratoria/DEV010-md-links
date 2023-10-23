0d3249b8da4deb2610d408c5d9f8299f17e23e39
Knelia <knelia1989@gmail.com>
2023-10-23 18:25:27 -0500

    Función mdLinks, se testea función de reject

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Aquí la función mdlinks que llamará a las otras funciones
+const fs = require('fs');
+
+const mdLinks = (ruta, options) => {
+  return new Promise((resolve, reject) => {
+  // Identificar si la ruta existe
+  if (fs.existsSync(ruta)){
+// revisar si la ruta es absoluta
+// probar si la ruta es absoluta 
+// si es un directorio filtrar los archivos md.
+  } else {
+    reject('Ruta no válida');
+  }
+  });
+};
+
+module.exports = {
+    mdLinks
+};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,14 @@
-const mdLinks = require('../');
+const { mdLinks } = require('../index.js');
 
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return a promise', () => {
+    expect(mdLinks).toBe(typeof Promise);
   });
+  it('reject when the path does not exist', () => {
+    return mdLinks('noexiste.md').catch((err) => {
+      expect(err).toBe('Ruta no válida');
+    })
+  })
 
 });
