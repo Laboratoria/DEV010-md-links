76b1b0446a35e770fb234652ee2a63d7d0363616
Knelia <knelia1989@gmail.com>
2023-10-25 17:29:50 -0500

    Modificación en el package y configuración de ecmaversion

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require('./lib/app');
 
-const mdLinks = (ruta) => {
-new Promise((resolve, reject)) => {
+function mdLinks(ruta) {
+new Promise((resolve, reject) => {
   const isAbsolute = app.isAbsolute(ruta);
   if (!isAbsolute) {
-    app. 
+    app.isAbsolute(ruta);
 // revisar si la ruta es absoluta
 // probar si la ruta es absoluta 
 // si es un directorio filtrar los archivos md.
@@ -12,7 +12,8 @@ new Promise((resolve, reject)) => {
     reject('Ruta no válida');
   }
   });
-});
+};
+
 module.exports = {
     mdLinks
 };
