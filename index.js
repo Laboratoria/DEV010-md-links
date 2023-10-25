42bb1e050b6356cb7041e1490aa21681ab782abc
karla aqueveque <aquevequekarla@gmail.com>
2023-10-25 18:31:50 -0300

    recursividad hito3

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 const mdLinksInstance = require("./lib/app");
 const readFileInstance = require("./lib/readfile");
+const { getFilesInDirectory } = require("./lib/readDirectory");
+const fsInstance = require("fs");
 
 function mdLinks(filePath, validate) {
   return new Promise((resolve, reject) => {
     // Comprueba si filePath no es una cadena
-    if (typeof filePath !== "string") {
+    if (typeof filePath !== "string" || filePath === "") {
       reject(new Error("Debe entregar una ruta de archivo válida"));
     }
+
+    // la exclamacion al inicio pregunta si es falso
+    if (!fsInstance.existsSync(filePath)) {
+      reject(new Error("La ruta indicada no existe"));
+    }
+
     // Obtiene la ruta absoluta del archivo utilizando la función "getAbsolutePath" de "mdLinksInstance"
     const absolutePath = mdLinksInstance.getAbsolutePath(filePath);
 
-    // Comprueba si la ruta absoluta es nula (indicando un archivo inválido)
-    if (absolutePath === null) {
-      reject(new Error("Debe entregar un archivo válido"));
+    if (fsInstance.lstatSync(absolutePath).isDirectory()) {
+      // es cuando filepath es un directorio
+      const filesMD = getFilesInDirectory(absolutePath);
+      resolve(filesMD);
+    } else {
+      // esto es cuando filePath es un archivo
+      if (!mdLinksInstance.isMarkdownFile(absolutePath)) {
+        // Comprueba si el archivo es de tipo Markdown utilizando la función "isMarkdownFile" de "mdLinksInstance"
+        reject(new Error("El archivo no es de tipo Markdown"));
+      }
+      // Llama a "findLinksInMarkdownFile" para buscar enlaces en el archivo y valida si es necesario
+      readFileInstance
+        .findLinksInMarkdownFile(absolutePath, validate)
+        .then((response) => {
+          resolve(response);
+        })
+        .catch((error) => {
+          reject(error);
+        });
     }
-    // Comprueba si el archivo es de tipo Markdown utilizando la función "isMarkdownFile" de "mdLinksInstance"
-    if (!mdLinksInstance.isMarkdownFile(absolutePath)) {
-      reject(new Error("El archivo no es de tipo Markdown"));
-    }
-    // Llama a "findLinksInMarkdownFile" para buscar enlaces en el archivo y valida si es necesario
-    readFileInstance
-      .findLinksInMarkdownFile(absolutePath, validate)
-      .then((response) => {
-        resolve(response);
-      })
-      .catch((error) => {
-        reject(error);
-      });
   });
 }
-// Ejemplos de uso de mdLinks con diferentes archivos y opciones de validación
+
 mdLinks("./example/probando3.md", true)
   .then((response) => {
     console.table(response);
@@ -46,13 +57,14 @@ mdLinks("./example/probando3.md")
     console.log(error);
   });
 
-mdLinks("./example/probando.js")
+mdLinks("./example/")
   .then((response) => {
     console.table(response);
   })
   .catch((error) => {
     console.log(error);
   });
+
 module.exports = {
   mdLinks,
 };
