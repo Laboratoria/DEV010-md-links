27c304dbdc1c9140766b10e788efeffdf75df951
Karina Navarro <karinavarro.b@gmail.com>
2023-10-24 21:00:43 -0600

    funciones isAbsolute,exist,readFile terminadas

---
+++ ReadmeExam.md
@@ -0,0 +1,10 @@
+## 1. Preámbulo
+
+[Markdown](https://es.wikipedia.org/wiki/Markdown) es un lenguaje de marcado
+
+Dentro de una comunidad de código abierto, nos han propuesto crear una
+herramienta usando [Node.js](https://nodejs.org/), que lea y analice archivos
+en formato `Markdown`, para verificar los links que contengan y reportar
+algunas estadísticas.
+
+![md-links](https://github.com/Laboratoria/bootcamp/assets/12631491/fc6bc380-7824-4fab-ab8f-7ab53cd9d0e4)
--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+
+const mdLinks = require("md-links");
+const { isAbsolute } = require("./lib/app");
+
+console.log('Hola Kari')
+mdLinks("./some/example.md")
+  .then(links => {
+    // => [{ href, text, file }, ...]
+  })
+  .catch(console.error);
+
+
 module.exports = () => {
   // ...
 };
+// aqui vive mdlinks, si se necesita una función se manda a llmar con un requiere
+function mdlinks = (route, options) => {
+  return new Promise((resolve,reject)=>{
+
+  })
+  const isAbsolute = pathFunctions.isAbsolute(route);}
+ if (!isAbsolute){
+  const absolute =pathFuncion.convertAbsolute(route)}
+
+
+  return new Promise((resolve, reject) => {
+  }
+}
+
+//para ver errores
+
+mdLinks('Readme.ms')
+.then (( result) =>{
+  console.log({Rresult})
+})
