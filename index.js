6b44e18cc644b96698ad1be4983224401e949b5e
alexaolalde <alejandra.olalde1817@gmail.com>
2023-10-27 14:34:57 -0600

    Se hacen las funciones mdlinks readfile verify extension

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
-};
+const { mdLinks } = require("./lib/mdlinks.js");
+
+mdLinks("./README.md")
+  .then((links) => {
+    console.log(links);
+    // => [{ href, text, file }, ...]
+  })
+  .catch(console.error);
+
+/* mdLinks("./thumb.png")
+  .then((links) => {
+    console.log(links);
+    // => [{ href, text, file }, ...]
+  })
+  .catch(console.error);
+ */
