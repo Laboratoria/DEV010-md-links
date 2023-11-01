8438c23ce5d23467b29f01fafc1a627d619f5d2a
Knelia <knelia1989@gmail.com>
2023-11-01 17:07:48 -0500

    Configuraciones de eslint y husky

--- cli.js
+++
@@ -1,8 +0,0 @@
-const { mdLinks } = require('./index.js');
-
-mdLinks('/noexiste/').then(() => {})
-.catch((err) => { 
-    console.log(err)
-});
-// then y catch 
-// va a leer los argumentos de líneas de comando y pasarlo a mdLink
