c40894384faf93bb6ef101efd20ca8617cfc10c3
Karina Navarro <karinavarro.b@gmail.com>
2023-11-02 23:45:56 -0600

    Se termino H1 con test, promesesa md-links

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 //const mdLinks = require("md-links");
 const app = require("./lib/app.js");
-const fs = require("fs");
-let colors = require('colors/safe');
+const fs = require("fs/promises");
+let colors = require("colors/safe");
+// const { readFile } = require("./lib/app.js");
+const { extLinksMD } = require("./lib/app.js");
 
 // mdLinks("./some/example.md")
 //   .then(links => {
@@ -10,30 +11,68 @@ let colors = require('colors/safe');
 //   })
 //   .catch(console.error);
 
+// const mdlinks = (route) => {
+//   return new Promise((resolve,reject)=>{
+//     if (app.isAbsolute(route)){
+//       return route
+//     } else{
+//     const absRouteTransform = app.convertPath(route);
+//     if(app.exist(absRouteTransform)){
+//       if(app.isCompExt(absRouteTransform)){
+//         resolve(absRouteTransform)
+//       } else {
+//         reject(colors.red("Error, extension is not compatible"))
+//       }
+//     } else {
+//       reject(colors.red("Route does not exists"))
+//     }
 
-const mdlinks = (route) => {
-  return new Promise((resolve,reject)=>{
-    if (app.isAbsolute(route)){
-      //regresar la ruta
-    } else{
+//   }})
+// }
+
+// mdlinks('lib/ReadmeExam.md')
+
+const mdlinks = (route,validate) => {
+  return new Promise((resolve, reject) => {
     const absRouteTransform = app.convertPath(route);
-    if(app.exist(absRouteTransform)){
-      if(app.isCompExt(absRouteTransform)){
-        resolve(absRouteTransform)
+    if (app.exist(absRouteTransform)) {
+      if (app.isCompExt(absRouteTransform)) {
+        extLinksMD(absRouteTransform)
+          .then((links) => {
+            console.log("Se extrajeron los siguientes links:", links);
+            return resolve(links);
+          })
+          .catch((error) => {
+           return console.log("Error al extraer los links", error);
+          });
       } else {
-        reject(colors.bgRed("Only .md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text' extensions"))
+        const error = new Error("Extension is not compatible");
+        console.log(colors.yellow(error.message));
+        reject(error);
       }
     } else {
-      reject(colors.red("Route does not exists"))
+      const error = new Error("Route does not exist");
+      console.log(colors.grey(error.message));
+      reject(error);
     }
-
-  }})
-}
-
-mdlinks('lib/ReadmeExam.md')
+  });
+};
 
 
 
+// mdlinks("ReadmeExam.md")
+//   .then((links) => {
+//     console.log(colors.rainbow("Los links son los siguientes:", links));
+//    return extLinksMD(links)
+//   })
+//   .catch((error) => {
+//     console.log("Error al extraer los links:", error);
+//   });
+// mdLinks("./some/example.md")
 
+// PROMISE ALL
+// Toma como argumento un array de promesas y retorna una sola promesa
+// si hay una promesa que no se cumple esta promesa se va a rechazar
+// si promise all se resuelve devuelve un array que conitene los valores de las promesas
 
-module.exports = {mdlinks};
+module.exports = { mdlinks };
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,6 +1,11 @@
-
-const {isAbsolute, exist, isCompExt, readFile, convertPath, extLinksMD} = require("../lib/app.js")
-
+const {
+  isAbsolute,
+  exist,
+  isCompExt,
+  readFile,
+  convertPath,
+  extLinksMD,
+} = require("../lib/app.js");
 
 // describe('mdLinks', () => {
 //   it('debería resolver un arreglo con 3 links para un archivo .md con 3 links', () => {
@@ -9,72 +14,87 @@ const {isAbsolute, exist, isCompExt, readFile, convertPath, extLinksMD} = requir
 //     });
 //   });
 // });
-describe("isAbsolute", ()=>{
-  const routeABS= 'C:/Laboratoria/Proyectos/DEV010-md-links/README.md'
-  const routeREL= 'README.md'
-  it("Retorna la ruta absoluta", ()=>{
+describe("isAbsolute", () => {
+  const routeABS = "C:/Laboratoria/Proyectos/DEV010-md-links/README.md";
+  const routeREL = "README.md";
+  it("Retorna la ruta absoluta", () => {
     expect(isAbsolute(routeABS)).toBe(true);
   });
 
-  it("Retorna la false en ruta relativa",()=>{
+  it("Retorna la false en ruta relativa", () => {
     expect(isAbsolute(routeREL)).toBe(false);
-  });
-})
-
-describe("convertPath", ()=>{
-    const routeREL= 'README.md'
-    const routeABS= 'C:\\Laboratoria\\Proyectos\\DEV010-md-links\\README.md'
-
-  it("Retorna la ruta convertida", ()=>{
-    expect(convertPath(routeREL)).toEqual(routeABS);
-  });
-})
-
-
-describe("exist",()=>{
-  const route = 'C:/Laboratoria/Proyectos/DEV010-md-links/README.md'
-  const routeFalse= 'C:/Laboratoria/Proyectos/README.md'
-  it("is a function",()=>{
-    expect(typeof exist).toBe("function");
-  });
-
-  it("deberia mostrar false porque no existe ruta", ()=>{
-    expect(exist(routeFalse)).toEqual(false);
-  });
-
-  it("deberia retornar true ",() => {
-    expect(exist(route)).toEqual(true);
-  });
-})
-
-
-describe("isCompExt",()=>{
-  const route= 'C:/Laboratoria/Proyectos/DEV010-md-links/README.pdf'
-
-  it("is a function",()=>{
-    expect(typeof exist).toBe("function");
-  });
-
-  it("deberia convertir a una ruta compatible", ()=>{
-    expect(isCompExt(route)).toEqual();
-  });
-
-})
-
-
-describe("readFile", () => {
-  it("debería ser una promesa", () => {
-    expect(typeof readFile).toBe("object");
   });
 });
 
-describe("extLinksMD", ()=> {
- const arrayLinks =
-  it("deberia ser una funcion", ()=>{
+describe("convertPath", () => {
+  const routeREL = "README.md";
+  const routeABS = "C:\\Laboratoria\\Proyectos\\DEV010-md-links\\README.md";
+
+  it("Retorna la ruta convertida", () => {
+    expect(convertPath(routeREL)).toEqual(routeABS);
+  });
+});
+
+describe("exist", () => {
+  const route = "C:/Laboratoria/Proyectos/DEV010-md-links/README.md";
+  const routeFalse = "C:/Laboratoria/Proyectos/README.md";
+  it("is a function", () => {
+    expect(typeof exist).toBe("function");
+  });
+
+  it("deberia mostrar false porque no existe ruta", () => {
+    expect(exist(routeFalse)).toEqual(false);
+  });
+
+  it("deberia retornar true ", () => {
+    expect(exist(route)).toEqual(true);
+  });
+});
+
+describe("isCompExt", () => {
+  const routeFalse = "C:/Laboratoria/Proyectos/DEV010-md-links/README.pdf";
+  const routeTrue = "C:/Laboratoria/Proyectos/DEV010-md-links/README.md";
+
+  it("is a function", () => {
+    expect(typeof exist).toBe("function");
+  });
+
+  it("deberia retornar false al no ser una ruta compatible", () => {
+    expect(isCompExt(routeFalse)).toBe(false);
+  });
+
+  // it("deberia retornar true al ser una ruta compatible", ()=>{
+  //   expect(isCompExt(routeTrue)).toEqual(routeTrue));
+  // });
+});
+
+describe("readFile", () => {
+  it("debería ser una promesa", () => {
+    expect(typeof readFile).toBe(typeof Promise);
+  });
+
+  it("debe leer el contenido de un archivo existente", async () => {
+    const testredfile = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const contenido = await readFile("testReadFile.md");
+    expect(contenido).toBe(testredfile);
+  });
+});
+
+describe("extLinksMD", () => {
+  it("deberia ser una funcion", () => {
     expect(typeof extLinksMD).toBe("function");
   });
 
-  it("deberia retornar un array con los links", ()=>{
-    expect(extLinksMD()).toEqual()
-  })
-})
+  const rutaPrueba = "testPrueba.md";
+  it("deberia retornar un array con los links", async () => {
+    const arrayLinks = [
+      {
+        href: "https://es.wikipedia.org/wiki/Markdown",
+        text: "Markdown",
+        file: "testPrueba.md",
+      },
+    ];
+    const result = await extLinksMD(rutaPrueba);
+    expect(result).toEqual(arrayLinks);
+  });
+});
