7344c435fbd6764964cd675dff14de1d5a95dea8
alexaolalde <alejandra.olalde1817@gmail.com>
2023-11-03 16:15:48 -0600

    Se hace la función de extraer links, devolverlos en un array y se resuelve la promesa

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const { mdLinks } = require("./lib/mdlinks.js");
+// const route = "";
+// const route = "./thumb.png"
+// const route = "./nonexist.jpg";
 
-mdLinks("./README.md")
+/* console.log(process.argv);
+const path = process.argv[2];
+const options = process.argv;
+const validate = !!options.includes("--validate");
+const stats = !!options.includes("--stats"); */
+
+mdLinks("./examples.md")
   .then((links) => {
     console.log(links);
     // => [{ href, text, file }, ...]
