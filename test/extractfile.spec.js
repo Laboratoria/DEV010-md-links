9261909c5095e032c3431a83a31a9b94ce885396
alexaolalde <alejandra.olalde1817@gmail.com>
2023-11-07 17:10:43 -0600

    Se hace el test de extraer links y se pasa con el 100%

--- test/extractfile.spec.js
+++ test/extractfile.spec.js
@@ -1,5 +1,28 @@
-describe("mdLinks", () => {
-  it("should...", () => {
-    console.log("FIX ME!");
+const extractMarkdownLinks = require("../lib/extractfile"); // Importa la función extractMarkdownLinks desde un archivo llamado "extractfile" en una ubicación relativa.
+
+describe("extractMarkdownLinks", () => {
+  // Comienza la descripción de las pruebas para la función extractMarkdownLinks.
+  it("should extract Markdown links from content", () => {
+    // Prueba 1: Debe extraer enlaces Markdown del contenido.
+    const content = "This is [a link](https://example.com) to an example site."; // Contenido de ejemplo que contiene un enlace Markdown.
+    const expectedLinks = [
+      // Lista de enlaces Markdown esperados en el contenido.
+      { linkText: "a link", linkUrl: "https://example.com" },
+    ];
+    // Verifica que la función extractMarkdownLinks devuelva los enlaces Markdown esperados.
+    expect(extractMarkdownLinks(content)).toEqual(expectedLinks);
+  });
+
+  it("should handle multiple links in content", () => {
+    // Prueba 2: Debe manejar varios enlaces en el contenido.
+    const content =
+      "Check out [Example](https://example.com) and [Google](https://google.com)."; // Contenido de ejemplo que contiene dos enlaces Markdown.
+    const expectedLinks = [
+      // Lista de enlaces Markdown esperados en el contenido.
+      { linkText: "Example", linkUrl: "https://example.com" },
+      { linkText: "Google", linkUrl: "https://google.com" },
+    ];
+    // Verifica que la función extractMarkdownLinks devuelva los enlaces Markdown esperados en el contenido con múltiples enlaces.
+    expect(extractMarkdownLinks(content)).toEqual(expectedLinks);
   });
 });
--- test/verifyextension.spec.js
+++ test/verifyextension.spec.js
@@ -1,8 +1,11 @@
-const mdwnExtension = require("../lib/verifyExtension");
+const mdwnExtension = require("../lib/verifyExtension"); // Importa la función mdwnExtension desde un archivo llamado "verifyExtension" en una ubicación relativa.
 
 describe("isMdwnExtension", () => {
+  // Comienza la descripción de las pruebas para la función mdwnExtension.
   it("should return true for valid Markdown extensions", () => {
+    // Prueba 1: Debe devolver true para extensiones de Markdown válidas.
     const validExtensions = [
+      // Lista de extensiones de archivo válidas para Markdown.
       ".md",
       ".markdown",
       ".mkd",
@@ -12,13 +15,16 @@ describe("isMdwnExtension", () => {
       ".mdtext",
       ".text",
     ];
+    // Itera a través de las extensiones válidas y verifica que la función mdwnExtension devuelva true para cada una de ellas.
     validExtensions.forEach((ext) => {
       expect(mdwnExtension(`file${ext}`)).toBe(true);
     });
   });
 
   it("should return false for invalid extensions", () => {
-    const invalidExtensions = [".txt", ".html", ".js"];
+    // Prueba 2: Debe devolver false para extensiones inválidas.
+    const invalidExtensions = [".txt", ".html", ".js"]; // Lista de extensiones de archivo inválidas.
+    // Itera a través de las extensiones inválidas y verifica que la función mdwnExtension devuelva false para cada una de ellas.
     invalidExtensions.forEach((ext) => {
       expect(mdwnExtension(`file${ext}`)).toBe(false);
     });
