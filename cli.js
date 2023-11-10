bd7b5cb7e94bb9d43a7e3765e2d7e8e0674835d4
DianaJ-Dev <134237911+DianaJ-Dev@users.noreply.github.com>
2023-11-10 00:22:10 -0500

    avance 2 hito 4

--- cli.js
+++ cli.js
@@ -7,33 +7,50 @@ const [,, ...args] = process.argv
 const validate = args.indexOf('--validate')// (indexOf) Busca un valor en especifico
 const stats = args.includes('--stats')
 
-if (validate !== -1) { // Si --validate se encuentra en los argumentos
-  args.splice(validate, 1) // Elimina --validate de los argumentos
-  mdLinks(args[0], true) // Ejecuta con validación
-    .then((result) => {
-      const formattedLinks = result.links.map((link) => `${link.file} ${link.href} ${link.ok} ${link.status} ${link.text}`)
-      console.log(formattedLinks.join('\n'))
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-} else if(stats){
+if (stats && validate !== -1){
+  mdLinks(args[0],false)
+  .then((result) => {
+  const totalLinks = result.links.length
+  const linkUnique = []
+  const totalinksFail = []
+  result.links.forEach((link)=> {
+    if(!linkUnique.includes(link.href)){
+      linkUnique.push(link.href)
+    }
+  
+    if ((link.status >= 200 && link.status < 400)){
+      totalinksFail.push(link.href)
+    }
+console.log(`hollll${totalinksFail}`)
+  })
+  const unique = linkUnique.length
+  const linkFail = totalinksFail.length
+
+
+  console.log(totalLinks)
+  console.log(`total: ${totalLinks}`)
+  console.log(`unique: ${unique}`)
+  console.log(`broken: ${linkFail}`)
+  })
+}
+ else if(stats){
   mdLinks(args[0], false)
   .then((result) => {
   const tolalLinks = result.links.length
   console.log(`total: ${tolalLinks}`)
   console.log(`unique: ${tolalLinks}`)
   })
-/*}else if (stats,validate){
-  mdLinks(args[0],false)
-  .then((result) => {
-  const totalLinks = result.links.length
-  const totalinksFail = result.links.length 
-  console.log(`total: ${totalLinks}`)
-  console.log(`unique: ${totalLinks}`)
-  console.log(`broken: ${totalinksFail}`)
-  })*/
-}else {
+}else if (validate !== -1) { // Si --validate se encuentra en los argumentos
+  args.splice(validate, 1) // Elimina --validate de los argumentos
+  mdLinks(args[0], true) // Ejecuta con validación
+    .then((result) => {
+      const formattedLinks = result.links.map((link) => `${link.file} ${link.href} ${link.ok || link.fail} ${link.status} ${link.text}`)
+      console.log(formattedLinks.join('\n'))
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+} else {
   mdLinks(args[0], false) // Ejecuta sin validación
     .then((result) => {
       const formattedLinks = result.links.map((link) => `${link.file}  ${link.href}  ${link.text}`)
--- example/markdown.md
+++ example/markdown.md
@@ -4,3 +4,5 @@ ligero muy popular entre developers.
 * Google  [Enlace a Google](https://www.google.com/).
 * Youtube [Enlace a youtube](https://www.youtube.com/).
 * Laboratoria  [Enlace a Laboratiria](https://www.laboratoria.la/).
+* Laboratoria  [Enlace a Laboratiria](https://www.laboratoria.la/).
+* Laboratoria  [Enlace a Laboratiria](https://www.laboratoria.la/).
